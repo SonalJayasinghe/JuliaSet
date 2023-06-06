2af5a75f8a02c997635b843ca60b137a41a86257
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2023-06-06 21:34:13 +0530

    JuliaSet Final Code

---
+++ src/main/java/org/example/ComplexNumbers.java
@@ -0,0 +1,62 @@
+package org.example;
+
+public class ComplexNumbers {
+    private double real;
+    private double imagine;
+
+    public ComplexNumbers(double real, double imagine) {
+        this.real = real;
+        this.imagine = imagine;
+    }
+
+    public ComplexNumbers add(ComplexNumbers c1){
+        double realPart = c1.real + this.real;
+        double imaginePart = c1.imagine + this.imagine;
+        return new ComplexNumbers(realPart,imaginePart);
+    }
+
+    public ComplexNumbers subtract(ComplexNumbers c1){
+        double realPart = c1.real - this.real;
+        double imaginePart = c1.imagine - this.imagine;
+        return new ComplexNumbers(realPart,imaginePart);
+    }
+
+    public ComplexNumbers multiply(ComplexNumbers c1){
+        double realPart = this.real* c1.real - this.imagine* c1.imagine;
+        double imaginePart = this.real * c1.imagine + this.imagine* c1.real;
+        return new ComplexNumbers(realPart,imaginePart);
+
+    }
+
+    public double magnitude(){
+        return (Math.sqrt(Math.pow(real,2)+Math.pow(imagine,2)));
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("""
+                %.02f + %.02fi""", this.real, this.imagine);
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public double getImagine() {
+        return imagine;
+    }
+
+    public void setImagine(double imagine) {
+        this.imagine = imagine;
+    }
+
+    public void setComplexNumber(ComplexNumbers complexNumber){
+        this.real = complexNumber.getReal();
+        this.imagine = complexNumber.getImagine();
+    }
+}
---
+++ src/main/java/org/example/JuliaSet.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class JuliaSet extends JFrame implements ActionListener {
+    private static final int MAX_ITERATION = 1000;
+    private static final double RADIUS = 3;   //Radius(R)  Choose R > 0 such that R**2 - R >= sqrt(cx**2 + cy**2)
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 800;
+
+    private final ComplexNumbers c = new ComplexNumbers(-0.75, 0.1); //Complex Number C
+
+    private boolean initialState = true;
+
+    public static void main(String[] args) {
+        new JuliaSet();
+    }
+
+    public JuliaSet() {
+        super("Julia Set");
+        JButton button = new JButton("Create");
+        setSize(WIDTH, HEIGHT);
+        add(button);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        button.setLayout(null);
+        button.setLocation(10, 10);
+        button.setSize(100, 25);
+        button.addActionListener(this);
+        setVisible(true);
+    }
+
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        if (!initialState) {
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    double zx = (x - WIDTH / 2.0) * 4.0 / WIDTH;    // scale to be between -R and R
+                    double zy = (y - HEIGHT / 2.0) * 4.0 / HEIGHT;  // scale to be between -R and R
+                    ComplexNumbers z = new ComplexNumbers(zx, zy);
+
+                    int iteration = 0;
+                    while (z.magnitude() < RADIUS*RADIUS && iteration < MAX_ITERATION) {
+                        z = z.multiply(z).add(c);
+                        iteration++;
+                    }
+
+                    int changeColor = 0; // Change this to get visible output to the screen
+                    if (iteration == MAX_ITERATION)
+                        g.setColor(Color.BLACK);
+                    else
+                        g.setColor(new Color(iteration - changeColor));
+                    g.fillRect(x, y, 1, 1);
+                }
+            }
+        }
+        initialState = false;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.repaint();
+    }
+}
